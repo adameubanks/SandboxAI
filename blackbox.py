5b64b6f601ff4d0c0312d983e5a7b014b34a3f14
Adam Eubanks <akhanrade@gmail.com>
2023-11-07 17:12:18 -0500

    make train/test split for xgboost

--- blackbox.py
+++ blackbox.py
@@ -1,14 +1,16 @@
+from sklearn.model_selection import train_test_split
 import xgboost as xgb
 import pred_actual
 
 def xgb_model(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     reg = xgb.XGBRegressor()
-    reg.fit(X, y)
-    y_pred = reg.predict(X)
+    reg.fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
 
-    image = pred_actual.graph(y, y_pred)
+    image = pred_actual.graph(y_test, y_pred)
 
-    r_score, mse = pred_actual.metrics(y, y_pred)
+    r_score, mse = pred_actual.metrics(y_test, y_pred)
 
     reg.save_model("xgboost_model.json")
 
